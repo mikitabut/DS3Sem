f3f7555b305703749357917a55688063c8424f7f
Nikita Butsko <nekete155@gmail.com>
2017-10-10 13:35:34 +0300

    Check for error

--- js/lab1.js
+++ js/lab1.js
@@ -3,69 +3,76 @@ class Drawer {
    * Create Drawer instance
    * @constructor
    */
-  constructor (canvasId) {
-    this.canvas = document.getElementById(canvasId)
-    this.context = this.canvas.getContext('2d')
-    this.halfOfX = Math.round(this.canvas.width / 2)
-    this.halfOfY = Math.round(this.canvas.height / 2)
-    this.context.translate(this.halfOfX, this.halfOfY)
-    this.context.rotate(-Math.PI / 2)
+  constructor (canvasId1, canvasId2) {
+    this.canvasDiff = document.getElementById(canvasId1)
+    this.contextDiff = this.canvasDiff.getContext('2d')
+    this.halfOfX = Math.round(this.canvasDiff.width / 2)
+    this.halfOfY = Math.round(this.canvasDiff.height / 2)
+    this.contextDiff.translate(this.halfOfX, this.halfOfY)
+    this.contextDiff.rotate(-Math.PI / 2)
+
+    this.canvasBrez = document.getElementById(canvasId2)
+    this.contextBrez = this.canvasBrez.getContext('2d')
+    this.contextBrez.translate(this.halfOfX, this.halfOfY)
+    this.contextBrez.rotate(-Math.PI / 2)
   }
 
   clear () {
     if (typeof this.halfOfX !== 'undefined') {
-      this.context.clearRect(-this.halfOfX, -this.halfOfY, this.halfOfX * 2, this.halfOfY * 2)
+      this.contextDiff.clearRect(-this.halfOfX, -this.halfOfY, this.halfOfX * 2, this.halfOfY * 2)
+      this.contextBrez.clearRect(-this.halfOfX, -this.halfOfY, this.halfOfX * 2, this.halfOfY * 2)
     }
   }
 
   setGrid (countOfXSquares, countOfYSquares) {
     this.x = countOfXSquares
     this.y = countOfYSquares
-    this.currentWidth = Math.round(this.canvas.width / this.x)
-    this.currentHeight = Math.round(this.canvas.height / this.y)
-    this.drawGrid()
+    this.currentWidth = Math.round(this.canvasDiff.width / this.x)
+    this.currentHeight = Math.round(this.canvasDiff.height / this.y)
+    this.drawGrid(this.contextDiff)
+    this.drawGrid(this.contextBrez)
   }
 
-  drawGrid () {
-    this.context.strokeStyle = '#FF0000'
+  drawGrid (context) {
+    context.strokeStyle = '#FF0000'
     for (let i = -this.halfOfY + this.currentHeight; i < this.halfOfY; i += this.currentHeight) {
       if (i === 0) {
-        this.context.strokeStyle = '#000000'
+        context.strokeStyle = '#000000'
       }
-      this.context.beginPath()
-      this.context.moveTo(-this.halfOfX, i)
-      this.context.lineTo(this.halfOfX, i)
-      this.context.stroke()
-      this.context.strokeStyle = '#FF0000'
+      context.beginPath()
+      context.moveTo(-this.halfOfX, i)
+      context.lineTo(this.halfOfX, i)
+      context.stroke()
+      context.strokeStyle = '#FF0000'
     }
     for (let i = -this.halfOfX + this.currentWidth; i < this.halfOfX; i += this.currentWidth) {
       if (i === 0) {
-        this.context.strokeStyle = '#000000'
+        context.strokeStyle = '#000000'
       }
-      this.context.beginPath()
-      this.context.moveTo(i, -this.halfOfY)
-      this.context.lineTo(i, this.halfOfY)
-      this.context.stroke()
-      this.context.strokeStyle = '#FF0000'
+      context.beginPath()
+      context.moveTo(i, -this.halfOfY)
+      context.lineTo(i, this.halfOfY)
+      context.stroke()
+      context.strokeStyle = '#FF0000'
     }
   }
 
-  setSquares (filledSquares) {
+  setSquares (filledSquares, context) {
     this.filledSquares = filledSquares
-    this.context.fillStyle = 'yellow'
+    context.fillStyle = 'yellow'
     for (let itemName in this.filledSquares) {
       let item = this.filledSquares[itemName]
-      this.context.fillRect(item.y * this.currentWidth, item.x * this.currentHeight,
+      context.fillRect(item.y * this.currentWidth, item.x * this.currentHeight,
         this.currentWidth, this.currentHeight)
     }
   }
 
-  setLine (x1, y1, x2, y2, color) {
-    this.context.beginPath()
-    this.context.strokeStyle = color
-    this.context.moveTo(y1 * this.currentWidth, x1 * this.currentHeight)
-    this.context.lineTo(y2 * this.currentWidth, x2 * this.currentHeight)
-    this.context.stroke()
+  setLine (x1, y1, x2, y2, color, context) {
+    context.beginPath()
+    context.strokeStyle = color
+    context.moveTo(y1 * this.currentWidth, x1 * this.currentHeight)
+    context.lineTo(y2 * this.currentWidth, x2 * this.currentHeight)
+    context.stroke()
   }
 
   draw (x1, y1, x2, y2, countOfX, countOfY) {
@@ -77,6 +84,7 @@ class Drawer {
     this.setGrid(countOfX, countOfY)
     let lines = []
     this.difAnalyzer(currenty, y2, currentx, x2, dy, dx, lines, x1, y1)
+    this.brezenhem(currenty, y2, currentx, x2, dy, dx, lines, x1, y1)
     for (let item in lines) {
       this.setLine(lines[item].x1, lines[item].y1, lines[item].x2, lines[item].y2, lines[item].color)
     }
@@ -103,7 +111,7 @@ class Drawer {
       this.setSquares([{
         x: Math.round(currentX) - 0.5,
         y: Math.round(currentY) - 0.5
-      }])
+      }], this.contextDiff)
 
       if (Math.round(currentX + diffX) * nextSignX <= x2 * nextSignX
         && Math.round(currentY + diffY) * nextSignY <= y2 * nextSignY
@@ -119,9 +127,37 @@ class Drawer {
       }
       currentY += diffY
       currentX += diffX
-      this.setLine(x1, y1, x2, y2, 'blue')
+      this.setLine(x1, y1, x2, y2, 'blue', this.contextDiff)
 
     }
+
+  }
+
+  brezenhem (currentY, y2, currentX, x2, dy, dx, lines, x1, y1) {
+    let diffX
+    let diffY
+    let t0 = -1 / 2
+    let k = dy / dx
+    while (currentY <= y2 && currentX <= x2) {
+      this.setSquares([{
+        x: Math.round(currentX) - 0.5,
+        y: Math.round(currentY) - 0.5
+      }], this.contextBrez)
+      t0 += k
+      if (t0 >= 0) {
+        diffX = 1
+        diffY = 1
+        t0 -= 1
+      } else {
+        diffX = 1
+        diffY = 0
+      }
+      currentY += diffY
+      currentX += diffX
+
+    }
+    this.setLine(x1, y1, x2, y2, 'blue', this.contextBrez)
+
   }
 
 }
